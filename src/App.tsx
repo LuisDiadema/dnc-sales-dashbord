58cf754e3cdcf8730239787d3304241653668897
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2025-07-21 17:54:50 -0300

    Create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
-  return <>Hello</>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<>LOGIN</>} />
+        <Route path="/cadastro" element={<>CADASTRO</>} />
+        <Route path="/home" element={<>HOME</>} />
+        <Route path="/leads" element={<>LEADS</>} />
+        <Route path="/perfil" element={<>PERFIL</>} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
